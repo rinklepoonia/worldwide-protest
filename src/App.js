d9af01e9f62229dfc35f965cb5b10f7026c780fb
rinklepoonia <rinklepoonia889@gmail.com>
2024-05-24 14:18:48 +0530

    make slider of app section , about-section done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import HeroSection from './components/HeroSection';
 import AppsSection from './components/AppsSection';
+import AboutSection from './components/AboutSection';
 
 function App() {
   return (
     <>
       <HeroSection />
       <AppsSection />
+      <AboutSection/>
     </>
   );
 }
