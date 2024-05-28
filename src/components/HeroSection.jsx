c6a827375705f03f3c360de589d3eddcfa64c4f7
rinklepoonia <rinklepoonia889@gmail.com>
2024-05-28 11:16:41 +0530

    update-hero-section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -17,14 +17,16 @@ function HeroSection() {
             <div className='pt_40'>
                 <div className='container'>
                     <div className='d-flex justify-content-between align-items-center'>
-                        <p className='ff_inter fw-extrabold text40_xl lh_48 color_white mb-0'>Gxxx Gxxxxxx</p>
+                        <a href="">
+                            <p className='ff_inter fw-extrabold text40_xl lh_48 color_white mb-0'>Gxxx Gxxxxxx</p>
+                        </a>
                         <ul className={`${nav ? "end-0" : "right_100"} mobile-view  d-flex align-items-center gap_46 ps-0 overFlowHidden`}>
                             <ul className='d-flex flex_column gap_20 ps-0'>
-                                <a href="https://www.dextools.io/app/en/pairs" target='_blank'><img src={DextoolImg} alt="DextoolImg" /></a>
-                                <a href="https://x.com/?lang=en" target='_blank'><img src={twitterImg} alt="twitterImg" /></a>
-                                <a href="https://telegram.org/" target='_blank'><img src={telegramImg} alt="telegramImg" /></a>
+                                <a href="https://www.dextools.io/app/en/pairs" target='_blank'><img className='zoom transition' src={DextoolImg} alt="DextoolImg" /></a>
+                                <a href="https://x.com/?lang=en" target='_blank'><img className='zoom transition' src={twitterImg} alt="twitterImg" /></a>
+                                <a href="https://telegram.org/" target='_blank'><img className='zoom transition' src={telegramImg} alt="telegramImg" /></a>
                             </ul>
-                            <button className='ff_inter fw-semibold text16_sm lh_19 color_white bg-transparent btn_border btn_borderRadius btn_padding text-nowrap'>Buy On Uniswap</button>
+                            <button className='ff_inter fw-semibold text16_sm lh_19 color_white bg-transparent btn_border btn_borderRadius btn_padding text-nowrap btn_hover transition'>Buy On Uniswap</button>
                         </ul>
                         <div className='menu d-lg-none z_6' onClick={() => setnav(!nav)}>
                             <span className=''></span>
@@ -40,11 +42,11 @@ function HeroSection() {
                         <div className="col-lg-6">
                             <h1 className='ff_inter fw-extrabold text108_5xl lh_130 color_white mb-0'>G<span className='color_orange'>G</span></h1>
                             <p className='ff_inter fw-semibold text20_md lh_24 max_w_538 color_white opacity_80 mb-0'>Join The <span className='color_orange'>Worldwide Blockchain</span> Protest Against Corrupt Government Bodies</p>
-                            <div className='d-flex flex_wrap align-items-center gap-lg-5 gap-sm-3 gap-2 py_62_32'>
-                                <button className='ff_inter fw-semibold text16_sm lh_19 color_black bg_orange border_transparent btn_borderRadius btn_padding text-nowrap'>Buy $GrayGensler</button>
-                                <button className='ff_inter fw-semibold text16_sm lh_19 color_white bg-transparent btn_border btn_borderRadius btn_padding text-nowrap'>How To Buy?</button>
+                            <div className='d-flex flex_wrap align-items-center gap_20 py_xl_62_32 py_30'>
+                                <button className='ff_inter fw-semibold text16_sm lh_19 color_black bg_orange border_transparent btn_borderRadius btn_padding text-nowrap black_btn_hover transition '>Buy $GrayGensler</button>
+                                <button className='ff_inter fw-semibold text16_sm lh_19 color_white bg-transparent btn_border btn_borderRadius btn_padding text-nowrap btn_hover transition '>How To Buy?</button>
                             </div>
-                            <button className='ff_inter fw-normal text20_md lh_30 color_white bg-transparent btn_border radius_102 padding_btn text-nowrap'>dwqjddj324dnewdne3dewrrets
+                            <button className='ff_inter fw-normal text20_md lh_30 color_white bg-transparent btn_border radius_102 padding_btn text-nowrap transition'>dwqjddj324dnewdne3dewrrets
                                 <img className='ms-4' src={DocImg} alt="DocImg" />
                             </button>
                         </div>
