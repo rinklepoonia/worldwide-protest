2bfed935224818318587c9111518a06f9756eb16
rinklepoonia <rinklepoonia889@gmail.com>
2024-05-30 09:47:58 +0530

    make app section with d-flex

--- src/components/AppsSection.jsx
+++ src/components/AppsSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import DextoolLogo from '../assets/images/svg/dextools_logo.svg'
+import DextoolLogo from '../assets/images/png/dextools.png'
 import telegramLogo from '../assets/images/svg/telegram_symbol.svg'
 import twitterLogo from '../assets/images/svg/x_logo.svg'
 
@@ -7,24 +7,18 @@ function AppsSection() {
     return (
         <div className='bg_orange h_136 mt_23 z-1 position-relative d-flex align-items-center h_136'>
             <div className='container custam_container'>
-                <div className="row">
-                    <div className="col-xl-4 col-6">
-                        <div className='d-flex align-items-center gap_sm_22 gap_10 justify-content-lg-start justify-content-center'>
-                            <a href="https://x.com/?lang=en" target='_blank'><img src={twitterLogo} alt="twitterLogo" /></a>
-                            <h2 className='ff_inter fw-semibold text48_xxl lh_58 color_white mb-0'>twitter</h2>
-                        </div>
+                <div className="d-flex justify-content-between flex-wrap">
+                    <div className='d-flex align-items-center gap_sm_22 gap_10'>
+                        <a href="https://x.com/?lang=en" target='_blank'><img className='max_w_46 max_w_35' src={twitterLogo} alt="twitterLogo" /></a>
+                        <h2 className='ff_inter fw-semibold text48_xxl lh_58 color_white mb-0'>twitter</h2>
                     </div>
-                    <div className="col-xl-4 col-6">
-                        <div className='d-flex align-items-center gap_sm_22 gap_10 justify-content-center'>
-                            <a href="https://telegram.org/" target='_blank'><img src={telegramLogo} alt="telegramLogo" /></a>
-                            <h2 className='ff_inter fw-semibold text48_xxl lh_58 color_white mb-0'>Telegram</h2>
-                        </div>
+                    <div className='d-flex align-items-center gap_sm_22 gap_10'>
+                        <a href="https://telegram.org/" target='_blank'><img className='max_w_46 max_w_35' src={telegramLogo} alt="telegramLogo" /></a>
+                        <h2 className='ff_inter fw-semibold text48_xxl lh_58 color_white mb-0'>Telegram</h2>
                     </div>
-                    <div className="col-xl-4 col-12 mt-xl-0 mt-2">
-                        <div className='d-flex align-items-center gap_sm_22 gap_10 justify-content-xl-center justify-content-center'>
-                            <a href="https://www.dextools.io/app/en/pairs" target='_blank'><img src={DextoolLogo} alt="DextoolLogo" /></a>
-                            <h2 className='ff_inter fw-semibold text48_xxl lh_58 color_white mb-0'>DEXTOOLS</h2>
-                        </div>
+                    <div className='d-flex align-items-center gap_sm_22 gap_10'>
+                        <a href="https://www.dextools.io/app/en/pairs" target='_blank'><img className='max_w_46 max_w_35' src={DextoolLogo} alt="DextoolLogo" /></a>
+                        <h2 className='ff_inter fw-semibold text48_xxl lh_58 color_white mb-0'>DEXTOOLS</h2>
                     </div>
                 </div>
             </div>
